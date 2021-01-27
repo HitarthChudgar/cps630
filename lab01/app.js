1f642793cbfd4ff60198b6da5fb72a1738e2ed6d
Hitarth <hitarthchudgar@gmail.com>
2021-01-26 21:01:17 -0500

    Added functionality and struc

---
+++ lab01/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    //images modified
+    $("#fadein1").click(function () {
+        $(".macbook").fadeIn();
+    });
+    $("#fadeout2").click(function () {
+        $(".macbook").fadeOut();
+    });
+    $("#fadein5").click(function () {
+        $(".iPad").fadeIn();
+    });
+    $("#fadeout6").click(function () {
+        $(".iPad").fadeOut();
+    });
+    $("#fadein9").click(function () {
+        $("iPhone").fadeIn();
+    });
+    $("#fadeout10").click(function () {
+        $(".iPhone").fadeOut();
+    });
+
+    //rows modified
+    $("#fadein3").click(function () {
+        $("#appleMac").fadeIn();
+    });
+    $("#fadeout4").click(function () {
+        $("#appleMac").fadeOut();
+    });
+    $("#fadein7").click(function () {
+        $("#appleiPad").fadeIn();
+    });
+    $("#fadeout8").click(function () {
+        $("#appleiPad").fadeOut();
+    });
+    $("#fadein11").click(function () {
+        $("#appleiPhone").fadeIn();
+    });
+    $("#fadeout12").click(function () {
+        $("#appleiPhone").fadeOut();
+    });
+});
