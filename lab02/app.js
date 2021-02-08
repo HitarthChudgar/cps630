3650efa100b5620668543a9a3625371a80d33a76
Hitarth <hitarthchudgar@gmail.com>
2021-02-08 02:32:06 -0500

    Fixed keyup storage issues

--- lab02/app.js
+++ lab02/app.js
@@ -1,32 +1,41 @@
-const dropdownGenre = document.getElementsByClassName('genre');
-const dropdownType[j].ledocument.getElementsByClassName('type');
-const dropdownSpec = document.getElementsByClassName('spec');
-const output = document.getElementById('output');
 const year = document.getElementById('year');
 const museum = document.getElementById('museum');
+const dropdownGenre = document.getElementsByClassName('genre');
+const dropdownType = document.getElementsByClassName('type');
+const dropdownSpec = document.getElementsByClassName('spec');
+const outputgenre = document.getElementById('outputgenre');
+const outputtype = document.getElementById('outputtype');
+const outputmuseum = document.getElementById('outputmuseum');
+const outputyear = document.getElementById('outputyear');
+const outputspec = document.getElementById('outputspec');
 
-year.addEventListener('keyup', () => {
-    output.innerHTML = `<span class="mx-2">Year: ${year.value}</span>
-            <span class="mx-2">Museum: ${museum.value}</span>
-            <span class="mx-2">Genre: </span>
-            <span class="mx-2">Type: </span>
-            <span class="mx-2">Specification: </span>`;
-})
-
-museum.addEventListener('keyup', () => {
-    output.innerHTML = `<span class="mx-2">Year: ${year.value}</span>
-            <span class="mx-2">Museum: ${museum.value}</span>
-            <span class="mx-2">Genre: </span>
-            <span class="mx-2">Type: </span>
-            <span class="mx-2">Specification: </span>`;
-})
 
 for (let i = 0; i < dropdownGenre.length; i++) {
     dropdownGenre[i].addEventListener('click', () => {
-       output.innerHTML = `<span class="mx-2">Year: ${year.value}</span>
-            <span class="mx-2">Museum: ${museum.value}</span>
-            <span class="mx-2">Genre: ${dropdownGenre[i].innerText}</span>
-            <span class="mx-2">Type: </span>
-            <span class="mx-2">Specification: </span>`; 
+        outputgenre.innerHTML = `
+            <span class="mx-2">Genre: ${dropdownGenre[i].innerText}</span>`;
     })
 }
+
+for (let i = 0; i < dropdownType.length; i++) {
+    dropdownType[i].addEventListener('click', () => {
+        outputtype.innerHTML = `
+            <span class="mx-2">Type: ${dropdownType[i].innerText}</span>`;
+    })
+}
+
+for (let i = 0; i < dropdownSpec.length; i++) {
+    dropdownSpec[i].addEventListener('click', () => {
+        outputspec.innerHTML = `
+            <span class="mx-2">Genre: ${dropdownSpec[i].innerText}</span>`;
+    })
+}
+
+
+year.addEventListener('keyup', () => {
+    outputyear.innerHTML = `<span class="mx-2">Year: ${year.value}</span>`;
+})
+
+museum.addEventListener('keyup', () => {
+    outputmuseum.innerHTML = `<span class="mx-2">Museum: ${museum.value}</span>`;
+})
